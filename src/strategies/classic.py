333c0047016e0061d996e3b52b3f01cf6ec133d6
Christine Wong <55353492+Lillliant@users.noreply.github.com>
2025-02-28 20:23:25 -0500

    Implement the 3 classic TFT variants

--- src/strategies/classic.py
+++ src/strategies/classic.py
@@ -1,1 +1,21 @@
 # TFT, TF2T, STFT
+# TODO: Take a look at the other Axelrod tournament strategies and whether they should be implemented in this module
+from util.type import COOPERATE, DEFECT
+
+def TFT(mem: list[int]):
+    if (len(mem) == 0):
+        return COOPERATE
+    return mem[-1] # there is at least one elements
+
+def TF2T(mem: list[int]):
+    if (len(mem) < 2):
+        return COOPERATE
+    elif (mem[-1] == DEFECT and mem[-2] == DEFECT):
+        return DEFECT
+    else:
+        return COOPERATE
+
+def STFT(mem: list[int]):
+    if (len(mem) == 0):
+        return DEFECT
+    return mem[-1]
