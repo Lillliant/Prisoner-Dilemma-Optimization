014addbcdf20d6bb593c354951b9e41dc4d101a7
Christine Wong <55353492+Lillliant@users.noreply.github.com>
2025-03-07 09:08:55 -0500

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,9 +1,10 @@
-# global variables used like macros
+# types
 COOPERATE = 0
 DEFECT = 1
 
-MEMORY_DEPTH=3
-ITERATION=100
+MEMORY_DEPTH = 3
+STRATEGY_LENGTH = int(4**MEMORY_DEPTH) + int(2*MEMORY_DEPTH)
+ITERATION = 1000
 
 # payoff matrix
 PAYOFF = {
@@ -12,3 +13,11 @@ PAYOFF = {
     (COOPERATE,DEFECT):(0, 5),
     (DEFECT, DEFECT):(1,1)
 }
+
+# parameters specific for genetic algorithm
+MUTATION_RATE=0.001
+POPULATION_SIZE=100
+
+# parameters for hill climbing
+
+# parameters for tabu search
