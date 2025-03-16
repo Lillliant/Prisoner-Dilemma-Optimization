de5b3cc44cdc643d444e772d6decfea74c2cfb60
Christine Wong <55353492+Lillliant@users.noreply.github.com>
2025-03-16 19:52:06 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from optimization.hill import hill_climbing
 from optimization.tabu import tabu_search
 from optimization.genetic import genetic
 from tournament.tournament import tournament
+from tournament.strategies import generate_encoding
 from pprint import pprint
 from tournament.player import Player
 
@@ -20,6 +21,7 @@ for strategy in human_strategy:
 if USE_HILL_CLIMBING:
     print("Hill Climbing")
     hill_climbing_strategy = hill_climbing()
+    # hill_climbing_strategy = hill_climbing(opponent_strategy=generate_encoding('TFT'))
     strategies.append(('HILL_CLIMBING', Player('ENCODED', hill_climbing_strategy)))
     print("Hill Climbing Complete")
     print("optimized strategy: ", hill_climbing_strategy)
@@ -39,20 +41,21 @@ if USE_GENETIC:
     print("optimized strategy: ", genetic_strategy)
 
 # Run the tournament with the optimized strategies and the original strategies
-# (Win, Tie, Loss)
-strategy_scores = {name: (0,0,0) for name, _ in strategies}
-for name_a, strategy_a in strategies:
-    for name_b, strategy_b in strategies:
+# (Win, Tie, Loss, Cumulative Score)
+strategy_scores = {name: (0,0,0,0) for name, _ in strategies}
+for i, (name_a, strategy_a) in enumerate(strategies):
+    for name_b, strategy_b in strategies[i:]: # competes against itself, but avoid repeated matches
         players = [strategy_a, strategy_b]
         scores = tournament(players)
+        #print(f"{name_a} vs {name_b}: {scores}")
         if (scores[0] < scores[1]): # strategy_b wins
-            strategy_scores[name_b] = (strategy_scores[name_b][0] + 1, strategy_scores[name_b][1], strategy_scores[name_b][2])
-            strategy_scores[name_a] = (strategy_scores[name_a][0], strategy_scores[name_a][1], strategy_scores[name_a][2] + 1)
+            strategy_scores[name_b] = (strategy_scores[name_b][0] + 1, strategy_scores[name_b][1], strategy_scores[name_b][2], strategy_scores[name_b][3] + scores[1])
+            strategy_scores[name_a] = (strategy_scores[name_a][0], strategy_scores[name_a][1], strategy_scores[name_a][2] + 1, strategy_scores[name_a][3] + scores[0])
         elif (scores[0] > scores[1]): # strategy_a wins
-            strategy_scores[name_a] = (strategy_scores[name_a][0] + 1, strategy_scores[name_a][1], strategy_scores[name_a][2])
-            strategy_scores[name_b] = (strategy_scores[name_b][0], strategy_scores[name_b][1], strategy_scores[name_b][2] + 1)
+            strategy_scores[name_a] = (strategy_scores[name_a][0] + 1, strategy_scores[name_a][1], strategy_scores[name_a][2], strategy_scores[name_a][3] + scores[0])
+            strategy_scores[name_b] = (strategy_scores[name_b][0], strategy_scores[name_b][1], strategy_scores[name_b][2] + 1, strategy_scores[name_b][3] + scores[1])
         else: # tie
-            strategy_scores[name_a] = (strategy_scores[name_a][0], strategy_scores[name_a][1] + 1, strategy_scores[name_a][2])
-            strategy_scores[name_b] = (strategy_scores[name_b][0], strategy_scores[name_b][1] + 1, strategy_scores[name_b][2])
+            strategy_scores[name_a] = (strategy_scores[name_a][0], strategy_scores[name_a][1] + 1, strategy_scores[name_a][2], strategy_scores[name_a][3] + scores[0])
+            strategy_scores[name_b] = (strategy_scores[name_b][0], strategy_scores[name_b][1] + 1, strategy_scores[name_b][2], strategy_scores[name_b][3] + scores[1])
 
 pprint(strategy_scores)
